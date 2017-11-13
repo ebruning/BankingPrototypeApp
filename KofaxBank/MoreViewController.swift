774d3c9e3e06a7f1344614e3240a5a1100f8a6a0
Rupali Ghate Kofax Account <rupali.ghate@kofax.com>
2017-11-13 20:01:07 +0530

    Added all the extra options from home screen(location, about, profile, contactUs, etc) on more tab.

--- KofaxBank/MoreViewController.swift
+++ KofaxBank/MoreViewController.swift
@@ -8,25 +8,113 @@
 
 import UIKit
 
-class MoreViewController: UIViewController {
+import MessageUI
+
+class MoreViewController: UIViewController, UITabBarControllerDelegate, MFMailComposeViewControllerDelegate {
+
+    
+    //MARK: Private variables
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
 
     override func viewWillAppear(_ animated: Bool) {
-        customizeNavigationBar()
+
+    }
+
+    
+    func tabBarController(_ tabBarController: UITabBarController, didSelect viewController: UIViewController) {
+        if viewController != self {
+            
+        }
     }
     
-    @IBAction func resetData(_ sender: UIButton) {
+    @IBAction func onInfoClicked(_ sender: UIButton) {
+        print("onInfoClicked")
         
+        showApplicationInformation()
     }
 
-    private func customizeNavigationBar() {
-        UIApplication.shared.statusBarStyle = UIStatusBarStyle.default
-        navigationController?.navigationBar.tintColor = UIColor.darkGray
+    private func showApplicationInformation() {
+        Utility.showAlert(onViewController: self, titleString: "Kofax Bank", messageString: "Version 1.0")
+    }
+    
+    
+    @IBAction func onUserProfileClicked(_ sender: UIButton) {
+        print("onUserProfileClicked")
+        
+        showUserProfile()
     }
 
+    @IBAction func onNotificationClicked(_ sender: UIButton) {
+             print("onNotificationClicked")
+        
+        Utility.showAlertWithCallback(onViewController: self, titleString: "You have received 1 message", messageString: "You are required to update your profile.\n\nYou can take picure of your valid ID to update your profile details.", positiveActionTitle: "Update Now", negativeActionTitle: "Later", positiveActionResponse: {
+            
+            
+        }, negativeActionResponse: {
+            
+            
+        })
+    }
+
+    @IBAction func onContactUsClicked(_ sender: UIButton) {
+        
+        print("onContactUsClicked")
+        showEmailComposer()
+    }
+    
+    @IBAction func onLocationClicked(_ sender: UIButton) {
+             print("onLocationClicked")
+        
+        showLocation()
+    }
+
+    @IBAction func onResetClicked(_ sender: UIButton) {
+             print("onResetClicked")
+    }
+
+
+    //Contact Us
+    private func showEmailComposer() {
+        if MFMailComposeViewController.canSendMail() {
+            let mailer = MFMailComposeViewController()
+            mailer.mailComposeDelegate = self
+            
+            mailer.setSubject("Customer: Lucy Tate")
+            mailer.setToRecipients(NSArray.init(object: "rupali.ghate@kofax.com") as? [String])
+            
+            mailer.setMessageBody("", isHTML: false)
+            
+            mailer.modalPresentationStyle = .formSheet
+            
+            present(mailer, animated: true, completion: nil)
+        }
+    }
+
+    func mailComposeController(_ controller: MFMailComposeViewController, didFinishWith result: MFMailComposeResult, error: Error?) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    
+    //Location
+    
+    private func showLocation() {
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "Map")
+        
+        present(vc, animated: true, completion: nil)
+    }
+    
+    
+    //User profile
+    private func showUserProfile() {
+        let storyboard = UIStoryboard.init(name: "Main", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "UserProfile")
+        
+        present(vc, animated: true, completion: nil)
+    }
+    
+    
 }
